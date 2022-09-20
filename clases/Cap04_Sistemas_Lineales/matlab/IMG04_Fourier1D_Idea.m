ca90e9408b3ff5706777f437dcf9fe6ae5feb7cc
Domingo Mery <domingo.mery@uc.cl>
2022-09-20 11:53:15 -0300

    *

--- clases/Cap04_Sistemas_Lineales/matlab/IMG04_Fourier1D_Idea.m
+++ clases/Cap04_Sistemas_Lineales/matlab/IMG04_Fourier1D_Idea.m
@@ -25,7 +25,7 @@ end
 K = 365;
 F = zeros(n,1);
 figure(2)
-subplot(3,1,1);plot(f(1:K))
+subplot(3,1,1);plot(f(1:K),'linewidth',2)
 axis([0 K -2 2]);
 title('f: original function')
 setw
@@ -34,7 +34,7 @@ for k=1:n
     figure(2)
     z = sin(k*f0*pit');
     q = z.*f;
-    subplot(3,1,2);plot(z(1:K));title(['b: basis frequency = ' num2str(k)])
+    subplot(3,1,2);plot(z(1:K),'linewidth',2);title(['b: basis frequency = ' num2str(k)])
     axis([0 K -2 2]);
     subplot(3,1,3);bar(q(1:K));
     axis([0 K -2 2]);
